c3023af31b01ec76d1f01335d39691906e6ae8ec
mechion <4387895+mechion@users.noreply.github.com>
2026-02-06 23:32:32 +0000

    chore: cleanup unused code and comments

--- commit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Commit the test file and the fix for case sensitivity
-git add lsps/manager/connection_manager.go lsps/manager/connection_manager_test.go
-git commit -m "Refine ConnectionManager: Add tests and fix case-sensitivity"
