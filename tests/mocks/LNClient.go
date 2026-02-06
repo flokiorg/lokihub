dd5490c5c3be3b0704a1ef8443cdc6e0e2481217
mechion <4387895+mechion@users.noreply.github.com>
2026-02-06 16:40:17 +0000

    test: update LNClient mock

--- tests/mocks/LNClient.go
+++ tests/mocks/LNClient.go
@@ -1327,8 +1327,9 @@ func (_c *MockLNClient_MakeHoldInvoice_Call) RunAndReturn(run func(ctx context.C
 }
 
 // MakeInvoice provides a mock function for the type MockLNClient
-func (_mock *MockLNClient) MakeInvoice(ctx context.Context, amount int64, description string, descriptionHash string, expiry int64, throughNodePubkey *string) (*lnclient.Transaction, error) {
-	ret := _mock.Called(ctx, amount, description, descriptionHash, expiry, throughNodePubkey)
+// MakeInvoice provides a mock function for the type MockLNClient
+func (_mock *MockLNClient) MakeInvoice(ctx context.Context, amount int64, description string, descriptionHash string, expiry int64, throughNodePubkey *string, lspJitChannelSCID *string, lspCltvExpiryDelta *uint16, lspFeeBaseMloki *uint64, lspFeeProportionalMillionths *uint32) (*lnclient.Transaction, error) {
+	ret := _mock.Called(ctx, amount, description, descriptionHash, expiry, throughNodePubkey, lspJitChannelSCID, lspCltvExpiryDelta, lspFeeBaseMloki, lspFeeProportionalMillionths)
 
 	if len(ret) == 0 {
 		panic("no return value specified for MakeInvoice")
@@ -1336,18 +1337,18 @@ func (_mock *MockLNClient) MakeInvoice(ctx context.Context, amount int64, descri
 
 	var r0 *lnclient.Transaction
 	var r1 error
-	if returnFunc, ok := ret.Get(0).(func(context.Context, int64, string, string, int64, *string) (*lnclient.Transaction, error)); ok {
-		return returnFunc(ctx, amount, description, descriptionHash, expiry, throughNodePubkey)
+	if returnFunc, ok := ret.Get(0).(func(context.Context, int64, string, string, int64, *string, *string, *uint16, *uint64, *uint32) (*lnclient.Transaction, error)); ok {
+		return returnFunc(ctx, amount, description, descriptionHash, expiry, throughNodePubkey, lspJitChannelSCID, lspCltvExpiryDelta, lspFeeBaseMloki, lspFeeProportionalMillionths)
 	}
-	if returnFunc, ok := ret.Get(0).(func(context.Context, int64, string, string, int64, *string) *lnclient.Transaction); ok {
-		r0 = returnFunc(ctx, amount, description, descriptionHash, expiry, throughNodePubkey)
+	if returnFunc, ok := ret.Get(0).(func(context.Context, int64, string, string, int64, *string, *string, *uint16, *uint64, *uint32) *lnclient.Transaction); ok {
+		r0 = returnFunc(ctx, amount, description, descriptionHash, expiry, throughNodePubkey, lspJitChannelSCID, lspCltvExpiryDelta, lspFeeBaseMloki, lspFeeProportionalMillionths)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*lnclient.Transaction)
 		}
 	}
-	if returnFunc, ok := ret.Get(1).(func(context.Context, int64, string, string, int64, *string) error); ok {
-		r1 = returnFunc(ctx, amount, description, descriptionHash, expiry, throughNodePubkey)
+	if returnFunc, ok := ret.Get(1).(func(context.Context, int64, string, string, int64, *string, *string, *uint16, *uint64, *uint32) error); ok {
+		r1 = returnFunc(ctx, amount, description, descriptionHash, expiry, throughNodePubkey, lspJitChannelSCID, lspCltvExpiryDelta, lspFeeBaseMloki, lspFeeProportionalMillionths)
 	} else {
 		r1 = ret.Error(1)
 	}
@@ -1366,13 +1367,17 @@ type MockLNClient_MakeInvoice_Call struct {
 //   - descriptionHash
 //   - expiry
 //   - throughNodePubkey
-func (_e *MockLNClient_Expecter) MakeInvoice(ctx interface{}, amount interface{}, description interface{}, descriptionHash interface{}, expiry interface{}, throughNodePubkey interface{}) *MockLNClient_MakeInvoice_Call {
-	return &MockLNClient_MakeInvoice_Call{Call: _e.mock.On("MakeInvoice", ctx, amount, description, descriptionHash, expiry, throughNodePubkey)}
+//   - lspJitChannelSCID
+//   - lspCltvExpiryDelta
+//   - lspFeeBaseMloki
+//   - lspFeeProportionalMillionths
+func (_e *MockLNClient_Expecter) MakeInvoice(ctx interface{}, amount interface{}, description interface{}, descriptionHash interface{}, expiry interface{}, throughNodePubkey interface{}, lspJitChannelSCID interface{}, lspCltvExpiryDelta interface{}, lspFeeBaseMloki interface{}, lspFeeProportionalMillionths interface{}) *MockLNClient_MakeInvoice_Call {
+	return &MockLNClient_MakeInvoice_Call{Call: _e.mock.On("MakeInvoice", ctx, amount, description, descriptionHash, expiry, throughNodePubkey, lspJitChannelSCID, lspCltvExpiryDelta, lspFeeBaseMloki, lspFeeProportionalMillionths)}
 }
 
-func (_c *MockLNClient_MakeInvoice_Call) Run(run func(ctx context.Context, amount int64, description string, descriptionHash string, expiry int64, throughNodePubkey *string)) *MockLNClient_MakeInvoice_Call {
+func (_c *MockLNClient_MakeInvoice_Call) Run(run func(ctx context.Context, amount int64, description string, descriptionHash string, expiry int64, throughNodePubkey *string, lspJitChannelSCID *string, lspCltvExpiryDelta *uint16, lspFeeBaseMloki *uint64, lspFeeProportionalMillionths *uint32)) *MockLNClient_MakeInvoice_Call {
 	_c.Call.Run(func(args mock.Arguments) {
-		run(args[0].(context.Context), args[1].(int64), args[2].(string), args[3].(string), args[4].(int64), args[5].(*string))
+		run(args[0].(context.Context), args[1].(int64), args[2].(string), args[3].(string), args[4].(int64), args[5].(*string), args[6].(*string), args[7].(*uint16), args[8].(*uint64), args[9].(*uint32))
 	})
 	return _c
 }
@@ -1382,7 +1387,7 @@ func (_c *MockLNClient_MakeInvoice_Call) Return(transaction *lnclient.Transactio
 	return _c
 }
 
-func (_c *MockLNClient_MakeInvoice_Call) RunAndReturn(run func(ctx context.Context, amount int64, description string, descriptionHash string, expiry int64, throughNodePubkey *string) (*lnclient.Transaction, error)) *MockLNClient_MakeInvoice_Call {
+func (_c *MockLNClient_MakeInvoice_Call) RunAndReturn(run func(ctx context.Context, amount int64, description string, descriptionHash string, expiry int64, throughNodePubkey *string, lspJitChannelSCID *string, lspCltvExpiryDelta *uint16, lspFeeBaseMloki *uint64, lspFeeProportionalMillionths *uint32) (*lnclient.Transaction, error)) *MockLNClient_MakeInvoice_Call {
 	_c.Call.Return(run)
 	return _c
 }
@@ -2034,3 +2039,4 @@ func (_c *MockLNClient_UpdateLastWalletSyncRequest_Call) RunAndReturn(run func()
 	_c.Run(run)
 	return _c
 }
+
